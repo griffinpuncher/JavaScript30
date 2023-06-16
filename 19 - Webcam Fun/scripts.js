5069505b87ce40aaca44d16f8fe3436d35a93f36
griffinpuncher <benjaminadams41@gmail.com>
2023-06-16 03:20:44 -0400

    Partial Solve of exercise 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,30 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+//document.addEventListener('DOMContentLoaded',()=>{
+                
+        const video = document.querySelector('.player');
+        const canvas = document.querySelector('.photo');
+        const ctx = canvas.getContext('2d');
+        const strip = document.querySelector('.strip');
+        const snap = document.querySelector('.snap');
+
+        function getVideo() {
+        navigator.mediaDevices.getUserMedia({video: true, audio: false})
+                .then(localMediaStream => {
+                        video.srcObject = localMediaStream;
+                        video.play();
+                }).catch(err => {
+                        console.log('Oops!');
+                });
+        }
+        
+        function paintToCanvas() {
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                
+                setInterval(()=>{
+                        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+                }, 10)
+        }
+        
+        getVideo();
+        paintToCanvas();
+//});
